3aeee8c4581a137a309806ff8688e44d9fb9aca2
lmy98129 <41624140@xs.ustb.edu.cn>
2019-04-07 22:07:47 +0800

    fix: change the correct IP in server&client

--- net-lab-rpc/client/client.cpp
+++ net-lab-rpc/client/client.cpp
@@ -26,7 +26,7 @@ int main(int argc, const char *argv[]) {
   // IPv4地址族
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(SERVER_PORT);
-  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_addr.s_addr = inet_addr("198.181.38.49");
   // sin_zero是为了让sockaddr与sockaddr_in两个数据结构保持大小相同而保留的空字节
   bzero(&(server_addr.sin_zero), 8);
 
